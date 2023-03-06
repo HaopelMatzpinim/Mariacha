c172c8a95dd1143b9bd305aa7fc587a5a325cbaa
Bob <127087070+Bob605@users.noreply.github.com>
2023-03-06 14:59:04 +0200

    feat(encryption_header.py): Added a function to insert an encryption header over a given packet.

---
+++ intercept_packages/encryption_header.py
@@ -0,0 +1,13 @@
+from scapy.all import StrFixedLenField, Packet, LongField
+
+SIGNATURE_SIZE = 32 
+
+class EncryptionHeader(Packet):
+    fields_desc = [
+        StrFixedLenField('signature', None, SIGNATURE_SIZE),
+        LongField('index', 0),
+    ]
+
+def insert_ecryption_header(packet, signature, index):
+    return packet / EncryptionHeader(signature=signature, index=index)
+
