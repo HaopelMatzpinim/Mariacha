2f0b1b33aefcb56c28fc1d8c77e889c3f805d64b
עזריאל יצחק אשר  ליבוביץ <u9039838@bsmch.net>
2023-03-07 15:43:47 +0200

    Feat(inercept_package.py): sort added

--- intercept_packages/intercept_package.py
+++ intercept_packages/intercept_package.py
@@ -1,13 +1,17 @@
+from scapy import packet
 from scapy.layers.inet import *
 from scapy.all import *
 from scapy.sendrecv import sendp, sniff
-from intercept_packages.encryption_header import EncryptionHeader
-from crypto.generate_key import generate_key
-from crypto.encrypt import encrypt_and_sign
-from crypto.decrypt import decrypt_and_verify
+from intercept_packages.encryption_header import *
+from crypto.generate_key import *
+from crypto.encrypt import *
+from crypto.decrypt import *
 
 INDEX = 0
-DEST_IP = '127.0.0.1'
+DEST_IP = '172.16.28.18'
+PLAIN_MAC = 'f4:39:09:10:b4:a8'
+ENCRYPTED_MAC = 'f4:39:09:10:b1:92'
+
 
 def encrypt(packet):
     build_packet = packet.build()
@@ -28,11 +32,9 @@ def plain_to_encrypted(packet):
     signature, encrypted_packet = encrypt(packet)
 
     packet_ready_to_send = Ether() \
-        / IP(dst=DEST_IP) \
-        / EncryptionHeader(signature=signature, index=INDEX) \
-        / Raw(encrypted_packet)
-
-    packet_ready_to_send.show()
+                           / IP(dst=DEST_IP) \
+                           / EncryptionHeader(magic=DEFAULT_HEADER_START, signature=signature, index=INDEX) \
+                           / Raw(encrypted_packet)
     INDEX += 1
     sendp(packet_ready_to_send)
 
@@ -40,12 +42,32 @@ def plain_to_encrypted(packet):
 def encrypted_to_plain(packet):
     try:
         raw = decrypt(packet)
-        packet_ready_to_send = Ether(dst=DEST_MAC) / Raw(raw)
+        packet_ready_to_send = Ether() \
+                               / IP(dst=DEST_IP) \
+                               / Raw(raw)
         sendp(packet_ready_to_send)
+    except (TypeError, AttributeError) as e:
+        packet.show()
+        print(type(e).__name__, e)
+    except DecryptionError as e:
+        print(type(e).__name__, e)
+    except OSError:
+        print(f'Error: package to long. len: {packet.len}')
+    except struct.error as e:
+        print(type(e).__name__, e)
     except Exception as e:
-        print(e)
+        print(f'unknown error. type: {type(e).__name__}, Error: {e}')
+
+
+def separate_in_and_out(pkt):
+    if pkt.dst == PLAIN_MAC:
+        plain_to_encrypted(pkt)
+    elif pkt.dst == ENCRYPTED_MAC and\
+            hasattr(packet, 'load') and\
+            len(packet.load) <= ENCRYPTION_HEADER_SIZE and\
+            not packet.load.startswith(DEFAULT_HEADER_START):
+        encrypted_to_plain(pkt)
 
 
 def sniff_all():
-    sniff(prn=plain_to_encrypted)
-    # sniff(prn=encrypted_to_plain)
+    sniff(prn=separate_in_and_out)
