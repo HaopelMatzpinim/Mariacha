1dafc2ac7e250dbe1ec30d90da174161c0b2eb34
עזריאל יצחק אשר  ליבוביץ <u9039838@bsmch.net>
2023-03-07 11:32:22 +0200

    Feat(all): incomming decrypted

--- crypto/decrypt.py
+++ crypto/decrypt.py
@@ -4,7 +4,7 @@ from hashlib import sha256
 def decrypt_and_verify(recBytes, signature, key):
     decrypted_xor = bytes(a ^ b for a, b in zip(recBytes, key))
 
-    if signature.digest() != sha256(decrypted_xor).digest():
+    if signature != sha256(decrypted_xor).digest():
         raise Exception("The signature that was recieved" +
                         " did not match the signature that was calculated!")
     else:
--- intercept_packages/encryption_header.py
+++ intercept_packages/encryption_header.py
@@ -2,6 +2,7 @@ from scapy.all import StrFixedLenField, Packet, LongField
 
 SIGNATURE_SIZE = 32
 
+
 class EncryptionHeader(Packet):
     fields_desc = [
         StrFixedLenField('signature', None, SIGNATURE_SIZE),
