35bea0102938c4f1963dc794a6e7b84edf3be282
עזריאל יצחק אשר  ליבוביץ <u9039838@bsmch.net>
2023-03-07 14:07:29 +0200

    Feat(all): Exception handling

--- intercept_packages/encryption_header.py
+++ intercept_packages/encryption_header.py
@@ -1,10 +1,16 @@
+import sys
+
 from scapy.all import StrFixedLenField, Packet, LongField
 
 SIGNATURE_SIZE = 32
 
+ENCRYPTION_HEADER_SIZE = SIGNATURE_SIZE + sys.getsizeof(int)
 
 class EncryptionHeader(Packet):
     fields_desc = [
         StrFixedLenField('signature', None, SIGNATURE_SIZE),
         LongField('index', 0),
     ]
+
+
+
--- intercept_packages/intercept_package.py
+++ intercept_packages/intercept_package.py
@@ -1,10 +1,10 @@
 from scapy.layers.inet import *
 from scapy.all import *
 from scapy.sendrecv import sendp, sniff
-from intercept_packages.encryption_header import EncryptionHeader
+from intercept_packages.encryption_header import *
 from crypto.generate_key import generate_key
 from crypto.encrypt import encrypt_and_sign
-from crypto.decrypt import decrypt_and_verify
+from crypto.decrypt import *
 
 INDEX = 0
 DEST_MAC = get_if_hwaddr(conf.iface)
@@ -27,22 +27,32 @@ def plain_to_encrypted(packet):
 
     signature, encrypted_packet = encrypt(packet)
 
-    packet_ready_to_send = Ether(type=0xDED, dst=DEST_MAC) \
-        / EncryptionHeader(signature=signature, index=INDEX) \
-        / Raw(encrypted_packet)
+    packet_ready_to_send = Ether(dst=DEST_MAC) \
+                           / EncryptionHeader(signature=signature, index=INDEX) \
+                           / Raw(encrypted_packet)
     INDEX += 1
     sendp(packet_ready_to_send)
 
 
 def encrypted_to_plain(packet):
     try:
+        if packet.len <= ENCRYPTION_HEADER_SIZE:
+            return
         raw = decrypt(packet)
         packet_ready_to_send = Ether(dst=DEST_MAC) / Raw(raw)
         sendp(packet_ready_to_send)
-    except Exception as e:
+    except (TypeError, AttributeError) as e:
         print(e)
+    except DecryptionError:
+        pass
+    except OSError:
+        print(f'Error: package to long. len: {packet.len}')
+    except struct.error:
+        pass
+    except Exception as e:
+        print(f'unknown error. type: {type(e).__name__}, Error: {e}')
 
 
 def sniff_all():
-    sniff(prn=plain_to_encrypted)
+    # sniff(prn=plain_to_encrypted)
     sniff(prn=encrypted_to_plain)
