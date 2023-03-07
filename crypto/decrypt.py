2a75eaa5b1571028dacb12963d6d55e53b9d4968
עזריאל יצחק אשר  ליבוביץ <u9039838@bsmch.net>
2023-03-07 15:43:47 +0200

    Feat(inercept_package.py): sort added

--- crypto/decrypt.py
+++ crypto/decrypt.py
@@ -1,11 +1,16 @@
 from hashlib import sha256
 
 
+class DecryptionError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
 def decrypt_and_verify(recBytes, signature, key):
     decrypted_xor = bytes(a ^ b for a, b in zip(recBytes, key))
 
     if signature != sha256(decrypted_xor).digest():
-        raise Exception("The signature that was recieved" +
-                        " did not match the signature that was calculated!")
+        raise DecryptionError("The signature that was received did not match the signature that was calculated!")
     else:
         return decrypted_xor
--- intercept_packages/encryption_header.py
+++ intercept_packages/encryption_header.py
@@ -1,10 +1,14 @@
 from scapy.all import StrFixedLenField, Packet, LongField
 
+DEFAULT_HEADER_START = b'abcd'
 SIGNATURE_SIZE = 32
+
+ENCRYPTION_HEADER_SIZE = len(DEFAULT_HEADER_START) + SIGNATURE_SIZE + 8
 
 
 class EncryptionHeader(Packet):
     fields_desc = [
+        StrFixedLenField('magic', DEFAULT_HEADER_START, len(DEFAULT_HEADER_START)),
         StrFixedLenField('signature', None, SIGNATURE_SIZE),
         LongField('index', 0),
     ]
